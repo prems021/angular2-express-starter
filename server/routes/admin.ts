b91e5e0b72264aa5dfa09451fbeb5ba22f2b3a02
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-13 14:30:17 +0530

    Update admin.ts

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -1,20 +1,13 @@
 import { Request, Response, Router } from "express";
-
+import { Admin_users } from '../models/admin_users';
 const adminRouter: Router = Router();
 
-
-adminRouter.get("/", (request: Request, response: Response) => {
-
-  response.json('hi');
-});
-
-
-
- userRouter.post('/login', (request: Request, response: Response) => {
+ 
+ adminRouter.post('/login', (request: Request, response: Response) => {
    var username = request.body.username;
    var password = request.body.password;
     
-    Hb_users.findOne({
+    Admin_users.findOne({
   where: {
     USER_NAME: request.body.username,
     PASSWORD: request.body.password
@@ -23,50 +16,15 @@ adminRouter.get("/", (request: Request, response: Response) => {
   
                        if(person)
                            { 
-                                  Super_admin.findOne({ where: { ADMIN_NAME : 'premji'}})
-                                                    .then(resu => {
-
-                                                      if (resu.DEMO_CLICKS < 0)
-                                                      {
-                                                         return response.json({success:false, msg:'Licence Expired Contact 7012406551'});
-                                                      }
-
-                                                      else
-                                                      {
-
-                                                         resu.update({ DEMO_CLICKS  : resu.DEMO_CLICKS - 1 })
-
-                                                           Hb_users.findOne({
-                                                                               where: {
-                                                                                         USER_NAME: username,
-                                                                                           
-                                                                                       }
-                                                                                     }).then(check => { 
-                                                                                           if( check.IS_ADMIN === true)
-                                                                                           {
-                                                                                             return response.json({success:true, msg:'Admin logged'});
-                                                                                           }
-
-                                                                                           else
-                                                                                           {
-                                                                                             return response.json({success:true, msg:'user logged'});
-                                                                                           }
-
-                                                                                     })
-                                                         
-
-                                                      } 
-                                                    })
-
+                                  return response.json({success:true, msg:'Admin logged'});
                             }
+                        else
+                        {
+                          return response.json({success:true, msg:'user logged'});
+                         }
+              })
 
-                            else
-                            {
-                               return response.json({success: false, msg: 'Authentication failed'});
-                            }
                             })
-
-                            })                        
 
                                                     
 
