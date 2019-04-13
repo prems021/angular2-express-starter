e78557a63965c6b4805c01c62ade605e2250d483
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-13 15:13:41 +0530

    Update admin_user.ts

--- server/model/admin_user.ts
+++ server/model/admin_user.ts
@@ -1,34 +1,76 @@
 import {  sequelize , Sequelize } from '../routes/dbcon';
-const dbcon_1 = require("../routes/dbcon");
-  const Admin_users = sequelize.define('amr_admin_users', {
+
+  const Hb_users = sequelize.define('hb_users', {
     ID: {
-      type: dbcon_1.Sequelize.INTEGER,
+      type: Sequelize.INTEGER(6),
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
     USER_NAME: {
-      type: dbcon_1.Sequelize.STRING(15),
+      type: Sequelize.STRING(15),
       allowNull: true
     },
     PASSWORD: {
-      type: dbcon_1.Sequelize.STRING(15),
+      type: Sequelize.STRING(15),
       allowNull: true
     },
-   
+      MAC: {
+      type: Sequelize.STRING(18),
+      allowNull: true
+           },
     
     IS_ADMIN: {
-      type: dbcon_1.Sequelize.BOOLEAN,
+      type: Sequelize.BOOLEAN,
       allowNull: true
     },
   
   }, {
-    tableName: 'amr_admin_users',
+    tableName: 'hb_users',
      timestamps: false
   });
 
 
-export {Admin_users};
+export {Hb_users};
+
+
+
+
+
+
+
+
+// import {  sequelize , Sequelize } from '../routes/dbcon';
+// const dbcon_1 = require("../routes/dbcon");
+//   const Admin_users = sequelize.define('amr_admin_users', {
+//     ID: {
+//       type: dbcon_1.Sequelize.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true
+//     },
+//     USER_NAME: {
+//       type: dbcon_1.Sequelize.STRING(15),
+//       allowNull: true
+//     },
+//     PASSWORD: {
+//       type: dbcon_1.Sequelize.STRING(15),
+//       allowNull: true
+//     },
+   
+    
+//     IS_ADMIN: {
+//       type: dbcon_1.Sequelize.BOOLEAN,
+//       allowNull: true
+//     },
+  
+//   }, {
+//     tableName: 'amr_admin_users',
+//      timestamps: false
+//   });
+
+
+// export {Admin_users};
 
 // "use strict";
 // Object.defineProperty(exports, "__esModule", { value: true });
