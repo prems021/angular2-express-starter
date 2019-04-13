ff4bf9eef460f6ff43a53b3fc33f9ddc5c6a8f62
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-13 13:11:49 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-
+import './polyfills';
+import 'hammerjs';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
