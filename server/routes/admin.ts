ab5d0e0b75db59ee391390af2c81e22644b4128c
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-13 17:44:39 +0530

    Update admin.ts

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import { Sequelize, sequelize } from './dbcon';
 import { Admin_users } from '../model/admin_user';
+import { Amr_out_users } from '../model/amr_out_users';
 const adminRouter: Router = Router();
 
 
@@ -20,6 +21,22 @@ adminRouter.get("/cdb_1", (request: Request, response: Response) => {
      
      })
  
+adminRouter.get("/cdb_2", (request: Request, response: Response) => {
+  
+   Amr_out_users.sync({force: true}).then(() => {
+   //Table created
+  return Amr_out_users.create({
+       
+     NAME: ''
+        
+    
+    })
+   })
+
+ return response.json({success:true, msg: 'CREATED'});
+     
+     })
+
  adminRouter.post('/login', (request: Request, response: Response) => {
    var username = request.body.username;
    var password = request.body.password;
