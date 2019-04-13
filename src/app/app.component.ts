8997275d711d7da2ea3298b1abec7c051486659b
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-13 13:08:10 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import { Observable } from 'rxjs/Observable';
-
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import { Store } from '@ngrx/store';
-import { IAppState } from './store';
-import { USER_GET } from './store/profile/profile.actions';
-import { ISimpleResponse } from './shared/interfaces/simple.interface';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent implements OnInit {
-
-  observable$: Observable<ISimpleResponse>;
-
-  constructor(private http: HttpClient, private store: Store<IAppState>) {}
-
-  ngOnInit() {
-
-    this.observable$ = this.http.get<ISimpleResponse>('/api/public/simple');
-
-    this.store.dispatch({
-      type: USER_GET
-    });
-  }
+export class AppComponent  {
+  name = 'Angular';
 }
+
