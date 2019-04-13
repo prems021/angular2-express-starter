006c91904e391c7aa14aea9c9dd7be9a67f4e0ec
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-13 14:22:52 +0530

    Create admin_user.ts

---
+++ server/model/admin_user.ts
@@ -0,0 +1,31 @@
+import {  sequelize , Sequelize } from '../routes/dbcon';
+
+  const Admin_users = sequelize.define('admin_users', {
+    ID: {
+      type: Sequelize.INTEGER(6),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    USER_NAME: {
+      type: Sequelize.STRING(15),
+      allowNull: true
+    },
+    PASSWORD: {
+      type: Sequelize.STRING(15),
+      allowNull: true
+    },
+   
+    
+    IS_ADMIN: {
+      type: Sequelize.BOOLEAN,
+      allowNull: true
+    },
+  
+  }, {
+    tableName: 'admin_users',
+     timestamps: false
+  });
+
+
+export {Admin_users};
