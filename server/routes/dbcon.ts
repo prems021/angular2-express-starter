377f3e8ad51ed47f61c3518335e1e4bbcb2af781
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-13 14:36:30 +0530

    Create dbcon.ts

---
+++ server/routes/dbcon.ts
@@ -0,0 +1,5 @@
+import { Sequelize } from "sequelize";
+
+ var sequelize = new Sequelize('postgres://xzqyirvy:Vlw2zQL-SVj5fTCDkskOguzkWEHn7Ea-@balarama.db.elephantsql.com:5432/xzqyirvy');
+
+export {   Sequelize, sequelize };
