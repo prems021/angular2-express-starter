e7b465135065146acfb228ead61134868fffd867
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-13 13:06:39 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,118 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http'; 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { routes } from './app.router';
-import { metaReducers, reducers } from './store';
-import { SharedModule } from './shared/shared.module';
-import { WeatherService } from './weather/weather.service';
-import { WeatherEffects } from './store/weather/weather.effects';
-import { FeedEffects } from './store/feed/feed.effects';
-import { ProfileEffects } from './store/profile/profile.effects';
-import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app.route';
+
+import { HeaderComponent } from './components/header/main';
+import { NavComponent } from './components/nav/main';
+import { FooterComponent } from './components/footer/main';
+
+import { LoginComponent } from './login/login';
+import { DashComponent } from './dash/main';
+
+import { AuthGuard } from './services/auth.guard';
+import { DataService } from './services/data.service';
+import { AuthenticationService } from './services/login.service';
+
+
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {A11yModule} from '@angular/cdk/a11y';
+import {
+  
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+  
+} from '@angular/material';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    SharedModule,
-    FormsModule,
+  imports:   [
+    AppRoutingModule,
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+    A11yModule,
+    CdkTableModule,
+    ScrollingModule,
+    CdkTreeModule,
+    DragDropModule,BrowserAnimationsModule,FormsModule,ReactiveFormsModule,HttpModule,
     HttpClientModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
-    EffectsModule.forRoot([
-      ProfileEffects,
-      FeedEffects,
-      WeatherEffects
-    ]),
-    !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : [],
-    RouterModule.forRoot(
-      routes,
-      {
-        useHash: true
-      }
-    )
-  ],
-  providers: [
-    WeatherService
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+     BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HeaderComponent,DashComponent,LoginComponent, NavComponent, FooterComponent ],
+  providers: [AuthenticationService,AuthGuard,DataService],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule { }
