ae63a49af8414fae58e4eb267519805076874cbd
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-13 15:10:28 +0530

    Update admin_user.ts

--- server/model/admin_user.ts
+++ server/model/admin_user.ts
@@ -1,61 +1,61 @@
-// import {  sequelize , Sequelize } from '../routes/dbcon';
-
-//   const Admin_users = sequelize.define('admin_users', {
-//     ID: {
-//       type: Sequelize.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true
-//     },
-//     USER_NAME: {
-//       type: Sequelize.STRING(15),
-//       allowNull: true
-//     },
-//     PASSWORD: {
-//       type: Sequelize.STRING(15),
-//       allowNull: true
-//     },
-   
-    
-//     IS_ADMIN: {
-//       type: Sequelize.BOOLEAN,
-//       allowNull: true
-//     },
-  
-//   }, {
-//     tableName: 'amr_admin_users',
-//      timestamps: false
-//   });
-
-
-// export {Admin_users};
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import {  sequelize , Sequelize } from '../routes/dbcon';
 const dbcon_1 = require("../routes/dbcon");
-const Admin_users = dbcon_1.sequelize.define('amr_admin_users', {
+  const Admin_users = sequelize.define('amr_admin_users', {
     ID: {
-        type: dbcon_1.Sequelize.INTEGER(15),
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+      type: dbcon_1.Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     USER_NAME: {
-        type: dbcon_1.Sequelize.INTEGER,
-        allowNull: true
+      type: dbcon_1.Sequelize.STRING(15),
+      allowNull: true
+    },
+    PASSWORD: {
+      type: dbcon_1.Sequelize.STRING(15),
+      allowNull: true
     },
    
-    PASSWORD: {
-        type: dbcon_1.Sequelize.STRING(25),
-        allowNull: true
-    },
-   IS_ADMIN: {
-        type: dbcon_1.Sequelize.BOOLEAN,
-        allowNull: true
+    
+    IS_ADMIN: {
+      type: dbcon_1.Sequelize.BOOLEAN,
+      allowNull: true
     },
   
-}, {
+  }, {
     tableName: 'amr_admin_users',
-    timestamps: false
-});
-exports.Admin_users = Admin_users;
+     timestamps: false
+  });
+
+
+export {Admin_users};
+
+// "use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+// const dbcon_1 = require("../routes/dbcon");
+// const Admin_users = dbcon_1.sequelize.define('amr_admin_users', {
+//     ID: {
+//         type: dbcon_1.Sequelize.INTEGER(15),
+//         allowNull: false,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     USER_NAME: {
+//         type: dbcon_1.Sequelize.INTEGER,
+//         allowNull: true
+//     },
+   
+//     PASSWORD: {
+//         type: dbcon_1.Sequelize.STRING(25),
+//         allowNull: true
+//     },
+//    IS_ADMIN: {
+//         type: dbcon_1.Sequelize.BOOLEAN,
+//         allowNull: true
+//     },
+  
+// }, {
+//     tableName: 'amr_admin_users',
+//     timestamps: false
+// });
+// exports.Admin_users = Admin_users;
