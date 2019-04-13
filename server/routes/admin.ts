e8f1f61df27d9eced090aa50fa8562ea3602c10c
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-13 14:44:21 +0530

    Update admin.ts

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { Admin_users } from '../model/admin_users';
+import { Admin_users } from './model/admin_users';
 const adminRouter: Router = Router();
 
 
