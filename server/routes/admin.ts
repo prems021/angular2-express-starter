421face33a0bec3272a599c1928ee67cb6c78581
prems021 <premji.netcom@gmail.com>
2019-04-15 21:48:41 +0530

    Update admin.ts

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -48,29 +48,8 @@ adminRouter.get("/cdb_2", (request: Request, response: Response) => {
      
      })
 
- adminRouter.post('/login', (request: Request, response: Response) => {
-   var username = request.body.username;
-   var password = request.body.password;
-    
-    Admin_users.findOne({
-  where: {
-    USER_NAME: request.body.username,
-    PASSWORD: request.body.password
-    }
-}).then(person => {
+
   
-                       if(person)
-                           { 
-                                  return response.json({success:true, msg:'Admin logged'});
-                            }
-                        else
-                        {
-                          return response.json({success:true, msg:'user logged'});
-                         }
-              })
-
-                            })
-
                                                     
 
 
