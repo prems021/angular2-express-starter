9ae2282b2a4066913e0c04885bf74326bd824b10
prems021 <premji.netcom@gmail.com>
2019-04-15 20:22:04 +0530

    Update invoice_slave.ts

--- server/model/invoice_slave.ts
+++ server/model/invoice_slave.ts
@@ -1,51 +1,51 @@
 import {  sequelize , Sequelize } from '../routes/dbcon';
-
+const dbcon_1 = require("../routes/dbcon");
   const Invoice_slave = sequelize.define('invoice_slave', {
     ID: {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },   
     MASTER_ID: {
-    type: Sequelize.INTEGER,
+    type: dbcon_1.Sequelize.INTEGER,
     allowNull: true
     }, 
        SI_NO: {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: true
     },
       PRODUCT_NAME: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
        HSN_CODE: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
     QUANTITY: {
-       type: Sequelize.INTEGER,
+       type: dbcon_1.Sequelize.INTEGER,
     allowNull: true
     },
        
       PRICE: {
-      type: Sequelize.FLOAT,
+      type: dbcon_1.Sequelize.FLOAT,
       allowNull: true
     },
   
      UNIT: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
     
    
     IS_B2B: {
-      type: Sequelize.BOOLEAN,
+      type: dbcon_1.Sequelize.BOOLEAN,
       allowNull: true
     },
 
       BILL_DATE : {
-        type : Sequelize.DATEONLY,
+        type : dbcon_1.Sequelize.DATEONLY,
         allowNull : true
       },
  
