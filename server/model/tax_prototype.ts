4140bf06dab5106ec7a59a455b3544135341ffca
prems021 <premji.netcom@gmail.com>
2019-04-15 20:25:00 +0530

    Update tax_prototype.ts

--- server/model/tax_prototype.ts
+++ server/model/tax_prototype.ts
@@ -1,30 +1,30 @@
 import {  sequelize , Sequelize } from '../routes/dbcon';
-
+const dbcon_1 = require("../routes/dbcon");
   const Tax_prototype = sequelize.define('tax_prototype', {
     ID: {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
     
    TAX_NAME : {
-    type: Sequelize.STRING,
+    type: dbcon_1.Sequelize.STRING,
     allowNull: true
    },
    
     TAX_DISPLAY_NAME : {
-    type: Sequelize.STRING,
+    type: dbcon_1.Sequelize.STRING,
     allowNull: true
    },
      
     TAX_RATE : {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: true
     },
 
     IS_ACTIVE : {
-      type: Sequelize.BOOLEAN,
+      type: dbcon_1.Sequelize.BOOLEAN,
       allowNull: true
     },
     
