5d3e1ad5fa5f5113b17eae178b2ffbdc524a36b2
prems021 <premji.netcom@gmail.com>
2019-04-15 22:32:07 +0530

    Update dbcon.ts

--- server/routes/dbcon.ts
+++ server/routes/dbcon.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
 
- var sequelize = new Sequelize('postgres://xnazucihybztsb:30b0add91b5ea0aa93eb3c8b91433449a209d4ea99df4e2ea1ac8c34cb2cc628@ec2-54-163-246-154.compute-1.amazonaws.com:5432/daav8etobj957o');
+ var sequelize = new Sequelize('postgres://jzwtwxsuneqaeu:bdf1f1efb4b7aaa50f8cb6a62dd8b54db3446f73ddbc3f32de48f645097ab9d5@ec2-50-19-127-115.compute-1.amazonaws.com:5432/dau39dgjg84tqd');
 
 export {   Sequelize, sequelize };
