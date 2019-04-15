77ad7890a6929db6cf908a135f808a4a56761dd8
prems021 <premji.netcom@gmail.com>
2019-04-15 20:18:10 +0530

    Update hb_customers.ts

--- server/model/hb_customers.ts
+++ server/model/hb_customers.ts
@@ -1,47 +1,47 @@
 import {  sequelize , Sequelize } from '../routes/dbcon';
-
+const dbcon_1 = require("../routes/dbcon");
   const Hb_customers = sequelize.define('hb_customers', {
     ID: {
-      type: Sequelize.INTEGER(15),
+      type: dbcon_1.Sequelize.INTEGER(15),
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
     CUSTOMER_NAME: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
     STREET: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
      CITY: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
     
     GSTIN: {
-      type: Sequelize.STRING(15),
+      type: dbcon_1.Sequelize.STRING(15),
       allowNull: true
     },
     PHONE: 
     {
-      type: Sequelize.STRING(15),
+      type: dbcon_1.Sequelize.STRING(15),
       allowNull : true
     },
       MOBILE: 
     {
-      type: Sequelize.STRING(15),
+      type: dbcon_1.Sequelize.STRING(15),
       allowNull : true
     },
         OPENING_BALANCE: 
     {
-      type: Sequelize.FLOAT,
+      type: dbcon_1.Sequelize.FLOAT,
       allowNull : true
     },
     LAST_PAYED_INVO_NUM:
     {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull : true
     },
     
