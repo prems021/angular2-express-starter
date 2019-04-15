95137a7df337da07ac4e5d5fdb96ff3d7bf235db
prems021 <premji.netcom@gmail.com>
2019-04-15 20:23:42 +0530

    Update super_admin.ts

--- server/model/super_admin.ts
+++ server/model/super_admin.ts
@@ -1,31 +1,31 @@
 import {  sequelize , Sequelize } from '../routes/dbcon';
-
+const dbcon_1 = require("../routes/dbcon");
   const Super_admin = sequelize.define('super_admin', {
     ID: {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
     ADMIN_NAME: {
-      type: Sequelize.STRING(8),
+      type: dbcon_1.Sequelize.STRING(8),
       allowNull: true
     },
     PASS_CODE: {
-      type: Sequelize.STRING(15),
+      type: dbcon_1.Sequelize.STRING(15),
       allowNull: true
     },
       ACTIVATION_KEY: {
-      type: Sequelize.STRING(25),
+      type: dbcon_1.Sequelize.STRING(25),
       allowNull: true
     },
     DEMO_CLICKS: {
-      type: Sequelize.INTEGER(5),
+      type: dbcon_1.Sequelize.INTEGER(5),
       allowNull: true
     },
     EXPIRY_DATE: 
     {
-      type: Sequelize.STRING(15),
+      type: dbcon_1.Sequelize.STRING(15),
       allowNull: true
     },
  
