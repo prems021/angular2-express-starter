1079e1dbab2551e4c241a36c6003dda1ccda7236
prems021 <premji.netcom@gmail.com>
2019-04-15 20:15:20 +0530

    Update cash_reciept.ts

--- server/model/cash_reciept.ts
+++ server/model/cash_reciept.ts
@@ -1,50 +1,50 @@
 import {  sequelize , Sequelize } from '../routes/dbcon';
-
+const dbcon_1 = require("../routes/dbcon");
   const Cash_reciept = sequelize.define('cash_reciept', {
     ID: {
-      type: Sequelize.INTEGER(15),
+      type: dbcon_1.Sequelize.INTEGER(15),
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
      RECIEPT_NO: {
-    type: Sequelize.INTEGER,
+    type: dbcon_1.Sequelize.INTEGER,
     allowNull: true
     },
     CUS_NAME: {
-      type: Sequelize.STRING(25),
+      type: dbcon_1.Sequelize.STRING(25),
       allowNull: true
     },
     CUS_STREET: {
-      type: Sequelize.STRING(25),
+      type: dbcon_1.Sequelize.STRING(25),
       allowNull: true
     },
       CASH_DATE: {
-      type: Sequelize.DATEONLY,
+      type: dbcon_1.Sequelize.DATEONLY,
       allowNull: true
     },
      IS_CASH_PAY: {
-      type: Sequelize.BOOLEAN,
+      type: dbcon_1.Sequelize.BOOLEAN,
       allowNull: true
     },
     AMOUNT_PAYED: {
-      type: Sequelize.DECIMAL(10, 2),
+      type: dbcon_1.Sequelize.DECIMAL(10, 2),
       allowNull: true
     },
     AMOUNT_DUE: {
-      type: Sequelize.DECIMAL(10, 2),
+      type: dbcon_1.Sequelize.DECIMAL(10, 2),
       allowNull: true
     },
      CHEQUE_NO: {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: true
     },
     CHEQUE_DATE: {
-      type: Sequelize.DATEONLY,
+      type: dbcon_1.Sequelize.DATEONLY,
       allowNull: true
     },
     BANK_NAME: {
-      type: Sequelize.STRING(25),
+      type: dbcon_1.Sequelize.STRING(25),
       allowNull: true
     },
 
