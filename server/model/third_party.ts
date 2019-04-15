5ca8e2332095297b0fab3a54cc8ad656f0fb9625
prems021 <premji.netcom@gmail.com>
2019-04-15 20:25:29 +0530

    Update third_party.ts

--- server/model/third_party.ts
+++ server/model/third_party.ts
@@ -1,24 +1,24 @@
 import {  sequelize , Sequelize } from '../routes/dbcon';
-
+const dbcon_1 = require("../routes/dbcon");
   const Third_party = sequelize.define('third_party', {
     ID: {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
   
     PARTY_NAME: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
 
       PARTY_ADDRESS: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
       PHONE_NUMBER: {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: true
     },
     
