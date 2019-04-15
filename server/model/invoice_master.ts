1d2ac4f04b43e2b4499ec42a43ce5fb21806e842
prems021 <premji.netcom@gmail.com>
2019-04-15 20:21:11 +0530

    Update invoice_master.ts

--- server/model/invoice_master.ts
+++ server/model/invoice_master.ts
@@ -1,100 +1,100 @@
 import {  sequelize , Sequelize } from '../routes/dbcon';
-
+const dbcon_1 = require("../routes/dbcon");
   const Invoice_master = sequelize.define('invoice_master', {
     ID: {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
     INVOICE_NUMBER: {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: true
     },
     CUSTOMER_NAME: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
       CUSTOMER_CITY: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
 
        CUSTOMER_STREET: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
      
      CUSTOMER_GST_IN: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
      
       CUSTOMER_PHONE: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
        CUSTOMER_MOBILE: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
       PAN_NO: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
 
       VEH_NO: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
 
       PLACE_SUPPLY: {
-      type: Sequelize.STRING,
+      type: dbcon_1.Sequelize.STRING,
       allowNull: true
     },
     BUNDLES : {
-     type: Sequelize.INTEGER,
+     type: dbcon_1.Sequelize.INTEGER,
      allowNull: true
     },
       BILL_DATE : {
-        type : Sequelize.DATEONLY,
+        type : dbcon_1.Sequelize.DATEONLY,
         allowNull : true
       },
 
       SUB_TOTAL: {
-      type: Sequelize.DECIMAL(10, 2),
+      type: dbcon_1.Sequelize.DECIMAL(10, 2),
       allowNull: true
     },
     
     TAX_COLLECTED: {
-      type: Sequelize.DECIMAL(10, 2),
+      type: dbcon_1.Sequelize.DECIMAL(10, 2),
       allowNull: true
     },
      GRAND_TOTAL: {
-      type: Sequelize.DECIMAL(10, 2),
+      type: dbcon_1.Sequelize.DECIMAL(10, 2),
       allowNull: true
     },
       ITEM_LENGTH: {
-      type: Sequelize.INTEGER,
+      type: dbcon_1.Sequelize.INTEGER,
       allowNull: true
     },
     
     TOTAL_PAYED: {
-      type: Sequelize.DECIMAL(10, 2),
+      type: dbcon_1.Sequelize.DECIMAL(10, 2),
       allowNull: true
     },
     TOTAL_DUE: {
-      type: Sequelize.DECIMAL(10, 2),
+      type: dbcon_1.Sequelize.DECIMAL(10, 2),
       allowNull: true
     },
      IS_PARTIAL_PAY: {
-      type: Sequelize.BOOLEAN,
+      type: dbcon_1.Sequelize.BOOLEAN,
       allowNull: true
     },
    
 
      IS_B2B: {
-      type: Sequelize.BOOLEAN,
+      type: dbcon_1.Sequelize.BOOLEAN,
       allowNull: true
     },
 
