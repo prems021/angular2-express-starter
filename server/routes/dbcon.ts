9e6d6f5d6615633d2ebdf1eb0b25b969ebca0d5f
prems021 <premji.netcom@gmail.com>
2019-04-15 19:58:39 +0530

    Update dbcon.ts

--- server/routes/dbcon.ts
+++ server/routes/dbcon.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
 
- var sequelize = new Sequelize('postgres://xzqyirvy:Vlw2zQL-SVj5fTCDkskOguzkWEHn7Ea-@balarama.db.elephantsql.com:5432/xzqyirvy');
+ var sequelize = new Sequelize('postgres://xnazucihybztsb:30b0add91b5ea0aa93eb3c8b91433449a209d4ea99df4e2ea1ac8c34cb2cc628@ec2-54-163-246-154.compute-1.amazonaws.com:5432/daav8etobj957o');
 
 export {   Sequelize, sequelize };
