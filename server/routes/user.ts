19566826dddb84346ee43236e486d46a7a4b93b6
prems021 <premji.netcom@gmail.com>
2019-04-16 12:49:31 +0530

    Update user.ts

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,7 +1,7 @@
 import { Request, Response, Router } from "express";
 import { Sequelize, sequelize } from './dbcon';
-// import { Hb_users } from '../model/users';
-// import { Super_admin } from '../model/super_admin';
+ import { Hb_users } from '../model/users';
+ import { Super_admin } from '../model/super_admin';
 const userRouter: Router = Router();
 
 userRouter.get("/", (request: Request, response: Response) => {
@@ -9,64 +9,63 @@ userRouter.get("/", (request: Request, response: Response) => {
   response.json('ji');
 });
 
-
-//  userRouter.post('/login_check', (request: Request, response: Response) => {
-//    var username = request.body.username;
-//    var password = request.body.password;
+ userRouter.post('/login_check', (request: Request, response: Response) => {
+   var username = request.body.username;
+   var password = request.body.password;
     
-//     Hb_users.findOne({
-//   where: {
-//     USER_NAME: request.body.username,
-//     PASSWORD: request.body.password
-//     }
-// }).then(person => {
+    Hb_users.findOne({
+  where: {
+    USER_NAME: request.body.username,
+    PASSWORD: request.body.password
+    }
+}).then(person => {
   
-//                        if(person)
-//                            { 
-//                                   Super_admin.findOne({ where: { ADMIN_NAME : 'premji'}})
-//                                                     .then(resu => {
+                       if(person)
+                           { 
+                                  Super_admin.findOne({ where: { ADMIN_NAME : 'premji'}})
+                                                    .then(resu => {
 
-//                                                       if (resu.DEMO_CLICKS < 0)
-//                                                       {
-//                                                          return response.json({success:false, msg:'Licence Expired Contact 7012406551'});
-//                                                       }
+                                                      if (resu.DEMO_CLICKS < 0)
+                                                      {
+                                                         return response.json({success:false, msg:'Licence Expired Contact 7012406551'});
+                                                      }
 
-//                                                       else
-//                                                       {
+                                                      else
+                                                      {
 
-//                                                          resu.update({ DEMO_CLICKS  : resu.DEMO_CLICKS - 1 })
+                                                         resu.update({ DEMO_CLICKS  : resu.DEMO_CLICKS - 1 })
 
-//                                                            Hb_users.findOne({
-//                                                                                where: {
-//                                                                                          USER_NAME: username,
+                                                           Hb_users.findOne({
+                                                                               where: {
+                                                                                         USER_NAME: username,
                                                                                            
-//                                                                                        }
-//                                                                                      }).then(check => { 
-//                                                                                            if( check.IS_ADMIN === true)
-//                                                                                            {
-//                                                                                              return response.json({success:true, msg:'Admin logged'});
-//                                                                                            }
+                                                                                       }
+                                                                                     }).then(check => { 
+                                                                                           if( check.IS_ADMIN === true)
+                                                                                           {
+                                                                                             return response.json({success:true, msg:'Admin logged'});
+                                                                                           }
 
-//                                                                                            else
-//                                                                                            {
-//                                                                                              return response.json({success:true, msg:'user logged'});
-//                                                                                            }
+                                                                                           else
+                                                                                           {
+                                                                                             return response.json({success:true, msg:'user logged'});
+                                                                                           }
 
-//                                                                                      })
+                                                                                     })
                                                          
 
-//                                                       } 
-//                                                     })
+                                                      } 
+                                                    })
 
-//                             }
+                            }
 
-//                             else
-//                             {
-//                                return response.json({success: false, msg: 'Authentication failed'});
-//                             }
-//                             })
+                            else
+                            {
+                               return response.json({success: false, msg: 'Authentication failed'});
+                            }
+                            })
 
-//                             })                        
+                            })                        
 
                                                     
 
@@ -77,39 +76,39 @@ userRouter.get("/", (request: Request, response: Response) => {
 
 
 
-//   userRouter.post('/server_check', (request: Request, response: Response) => {
-//    var username = 'a'
-//    var password = 's'
+  userRouter.post('/server_check', (request: Request, response: Response) => {
+   var username = 'a'
+   var password = 's'
    
-//     Hb_users.findOne({
-//   where: {
-//     USER_NAME: username,
-//     PASSWORD: password,
+    Hb_users.findOne({
+  where: {
+    USER_NAME: username,
+    PASSWORD: password,
    
-//     }
-// }).then(function(result){
+    }
+}).then(function(result){
   
-//                                                 if(result)
-//                                                 { 
-//                                                     if(result.IS_ADMIN === true)
-//                                                     {
-//                                                         return response.json({ msg:'server running'});
-//                                                        }
-//                                                           else
-//                                                 {
-//                                                      return response.json({ msg:'waiting'});   }
+                                                if(result)
+                                                { 
+                                                    if(result.IS_ADMIN === true)
+                                                    {
+                                                        return response.json({ msg:'server running'});
+                                                       }
+                                                          else
+                                                {
+                                                     return response.json({ msg:'waiting'});   }
 
-//                                                 }   
-//                                                 else
-//                                                 {
+                                                }   
+                                                else
+                                                {
 
-//                                                 response.status(403).send({msg: 'failed'});
-//                                                 }    
-//                          });
+                                                response.status(403).send({msg: 'failed'});
+                                                }    
+                         });
       
    
  
 
    
-//  });
+ });
 export { userRouter };
