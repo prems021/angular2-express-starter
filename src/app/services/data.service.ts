fa7fb11b609cb0361224eb4ae0ed8f0269cdb1a0
prems021 <premji.netcom@gmail.com>
2019-04-16 13:22:58 +0530

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -20,7 +20,7 @@ export class DataService {
 
     get_customers_subs_list()
     {
-      return this.http.get('https://coresoft.herokuapp.com/api/public/get_subscription_list',{headers : contentHeaders})  
+      return this.http.get('/api/public/get_subscription_list',{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
@@ -29,7 +29,7 @@ export class DataService {
      
        let body= JSON.stringify(model); 
        console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/cus_slave_report',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/cus_slave_report',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
    
@@ -37,13 +37,13 @@ export class DataService {
    {
       let body= JSON.stringify(model); 
        console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/cus_mas_report',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/cus_mas_report',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
    }
    
    get_reciept_number_b2b()
     {
-      return this.http.get('http://localhost:8081/api/public/get_reciept_number_b2b',{headers : contentHeaders})  
+      return this.http.get('/api/public/get_reciept_number_b2b',{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
@@ -52,7 +52,7 @@ export class DataService {
     {
        let body= JSON.stringify(model); 
        console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/post_cash_reciept',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/post_cash_reciept',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
     
@@ -61,7 +61,7 @@ export class DataService {
 
      let body= JSON.stringify(model); 
        console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/sales_report',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/sales_report',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
  } 
 
@@ -70,7 +70,7 @@ export class DataService {
 
      let body = JSON.stringify(model); 
        console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/get_report_expense',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/get_report_expense',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
 
   }     
@@ -80,7 +80,7 @@ export class DataService {
 
      let body = JSON.stringify(model); 
        console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/reports_purchase',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/reports_purchase',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
 
   }  
@@ -91,7 +91,7 @@ export class DataService {
 
      let body = JSON.stringify(model); 
        console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/reports_revenue',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/reports_revenue',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
 
   }  
@@ -101,7 +101,7 @@ export class DataService {
   {
      let body = JSON.stringify(model); 
        console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/add_other_expence_entry',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/add_other_expence_entry',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
 
   }
@@ -109,20 +109,20 @@ export class DataService {
 
     get_exp_type_list()
     {
-            return this.http.get('http://localhost:8081/api/public/list_expence_category',{headers : contentHeaders})  
+            return this.http.get('/api/public/list_expence_category',{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
 
    get_party_list()
    {
-      return this.http.get('http://localhost:8081/api/public/list_party',{headers : contentHeaders})  
+      return this.http.get('/api/public/list_party',{headers : contentHeaders})  
       .map((res: Response) => res.json())
    }
 
     get_exp_rec_no()
      {
-      return this.http.get('http://localhost:8081/api/public/get_exp_rec_no',{headers : contentHeaders})  
+      return this.http.get('/api/public/get_exp_rec_no',{headers : contentHeaders})  
       .map((res: Response) => res.json())
      }
 
@@ -131,7 +131,7 @@ export class DataService {
 
     let body = JSON.stringify(model); 
        console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/add_expence_entry',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/add_expence_entry',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
 
       }   
@@ -140,7 +140,7 @@ export class DataService {
     add_expense_category(model:any)
     {
       let body = JSON.stringify(model); 
-      return this.http.post('http://localhost:8081/api/public/add_expense_category',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/add_expense_category',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
@@ -148,7 +148,7 @@ export class DataService {
     {
       let body = JSON.stringify(model); 
       console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/delete_customer',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/delete_customer',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
@@ -156,18 +156,18 @@ export class DataService {
     {
       let body = JSON.stringify(model); 
       console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/update_customer',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/update_customer',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
     get_customer_list_all()
     {
-        return this.http.get('http://localhost:8081/api/public/get_customer_list_all',{headers : contentHeaders})  
+        return this.http.get('/api/public/get_customer_list_all',{headers : contentHeaders})  
          .map((res: Response) => res.json())
     }
       get_customer_list_b2b()
     {
-        return this.http.get('http://localhost:8081/api/public/get_customer_list_b2b',{headers : contentHeaders})  
+        return this.http.get('/api/public/get_customer_list_b2b',{headers : contentHeaders})  
          .map((res: Response) => res.json())
     }
 
@@ -175,7 +175,7 @@ export class DataService {
     {
          let body = JSON.stringify(model); 
       console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/add_customer',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/add_customer',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
@@ -183,25 +183,25 @@ export class DataService {
     {
           let body = JSON.stringify(model); 
       console.log('body',body);
-      return this.http.post('http://localhost:8081/api/post/update_invoice_b2b',body,{headers : contentHeaders})  
+      return this.http.post('/api/post/update_invoice_b2b',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
         update_invoice_b2c(model:any)
     {
           let body = JSON.stringify(model); 
       console.log('body',body);
-      return this.http.post('http://localhost:8081/api/post/update_invoice_b2c',body,{headers : contentHeaders})  
+      return this.http.post('/api/post/update_invoice_b2c',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
       get_invoices_b2b()
     {
-      return this.http.get('http://localhost:8081/api/public/get_invoice_list_b2b',{headers : contentHeaders})  
+      return this.http.get('/api/public/get_invoice_list_b2b',{headers : contentHeaders})  
          .map((res: Response) => res.json())
     }
     get_invoices_b2c()
      {
-      return this.http.get('http://localhost:8081/api/public/get_invoice_list_b2c',{headers : contentHeaders})  
+      return this.http.get('/api/public/get_invoice_list_b2c',{headers : contentHeaders})  
          .map((res: Response) => res.json())
     }
 
@@ -209,7 +209,7 @@ export class DataService {
     {
        let body = JSON.stringify(model); 
       console.log('body',body);
-      return this.http.post('http://localhost:8081/api/post/add_product',body,{headers : contentHeaders})  
+      return this.http.post('/api/post/add_product',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
@@ -217,27 +217,27 @@ export class DataService {
     {
       let body = JSON.stringify(model); 
       console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/update_product',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/update_product',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
    get_product_list()
    {
      
-      return this.http.get('http://localhost:8081/api/public/get_product_list',{headers : contentHeaders})  
+      return this.http.get('/api/public/get_product_list',{headers : contentHeaders})  
      .map((res: Response) => res.json())
 
    }
 
     get_active_taxes_only()
     {
-      return this.http.get('http://localhost:8081/api/public/get_active_taxes_only',{headers : contentHeaders})  
+      return this.http.get('/api/public/get_active_taxes_only',{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
     get_active_taxes_on_edit(model:any)
     {
        let body = JSON.stringify(model); 
-      return this.http.post('http://localhost:8081/api/public/get_active_taxes_on_edit',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/get_active_taxes_on_edit',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
@@ -245,7 +245,7 @@ export class DataService {
     {
       let body = JSON.stringify(model); 
       console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/disable_tax_cat',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/disable_tax_cat',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
@@ -253,13 +253,13 @@ export class DataService {
     {
       let body = JSON.stringify(model); 
       console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/enable_tax_cat',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/enable_tax_cat',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
     get_tax_cat()
     {
-      return this.http.get('http://localhost:8081/api/public/get_tax_cat',{headers : contentHeaders})  
+      return this.http.get('/api/public/get_tax_cat',{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
@@ -268,27 +268,27 @@ export class DataService {
       
       let body = JSON.stringify(model); 
       console.log('body',body);
-      return this.http.post('http://localhost:8081/api/public/add_tax_cat',body,{headers : contentHeaders})  
+      return this.http.post('/api/public/add_tax_cat',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
     get_greetings()
     {
-      return this.http.get('http://localhost:8081/api/public/get_greetings',{headers : contentHeaders})  
+      return this.http.get('/api/public/get_greetings',{headers : contentHeaders})  
          .map((res: Response) => res.json())
     }
    
    add_new_greeting(model:any)
    {
      let body= JSON.stringify(model); 
-      return this.http.post('http://localhost:8081/api/post/add_new_greeting', body, { headers: contentHeaders})
+      return this.http.post('/api/post/add_new_greeting', body, { headers: contentHeaders})
             .map((res: Response) => res.json())
    }
    update_default_greeting(model:any)
    {
      let body= JSON.stringify(model); 
                  console.log('bodies',body);
-      return this.http.post('http://localhost:8081/api/post/update_default_greeting', body, { headers: contentHeaders})
+      return this.http.post('/api/post/update_default_greeting', body, { headers: contentHeaders})
             .map((res: Response) => res.json())
    }
 
@@ -297,7 +297,7 @@ export class DataService {
        {
         let body= JSON.stringify(model); 
                  console.log('body',body);
-      return this.http.post('http://localhost:8081/api/post/post_invoice_b2b', body, { headers: contentHeaders})
+      return this.http.post('/api/post/post_invoice_b2b', body, { headers: contentHeaders})
             .map((res: Response) => res.json())
             
         } 
@@ -306,20 +306,20 @@ export class DataService {
   {
     
        let body = JSON.stringify(model); 
-        return this.http.post('http://localhost:8081/api/public/number_to_word',body,{headers : contentHeaders})  
+        return this.http.post('/api/public/number_to_word',body,{headers : contentHeaders})  
       .map((res: Response) => res.json())
   }
 
    get_invo_number_b2b()
     {
-         return this.http.get('http://localhost:8081/api/public/get_invoice_number_b2b',{headers : contentHeaders})  
+         return this.http.get('/api/public/get_invoice_number_b2b',{headers : contentHeaders})  
          .map((res: Response) => res.json())
     }
 
     
     get_products_list()
     {
-       return this.http.get('http://localhost:8081/api/public/get_products_list',{headers : contentHeaders})  
+       return this.http.get('/api/public/get_products_list',{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
  
@@ -328,7 +328,7 @@ export class DataService {
 
    get_customers_list_b2b()
    {
-     return this.http.get('http://localhost:8081/api/public/get_customers_list_b2b',{headers : contentHeaders})  
+     return this.http.get('/api/public/get_customers_list_b2b',{headers : contentHeaders})  
       .map((res: Response) => res.json())
    }
 
@@ -337,7 +337,7 @@ export class DataService {
         let body= JSON.stringify(model); 
 
         
-        return this.http.post('http://localhost:8081/api/public/add_user', body, { headers: contentHeaders})
+        return this.http.post('/api/public/add_user', body, { headers: contentHeaders})
             .map((response: Response) => {
                 
                 
