bdd2e960a1a6e29ceeb720518e33779beca97634
prems021 <premji.netcom@gmail.com>
2019-04-16 12:20:15 +0530

    Update admin.ts

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -16,13 +16,14 @@ import { Cash_reciept} from '../model/cash_reciept';
 const adminRouter: Router = Router();
 
 
-adminRouter.get("/cdb_1", (request: Request, response: Response) => {
+
+  adminRouter.get("/cash_reciept", (request: Request, response: Response) => {
   
-   Hb_users.sync({force: true}).then(() => {
+   Cash_reciept.sync({force: true}).then(() => {
    //Table created
-  return Hb_users.create({
+  return Cash_reciept.create({
        
-     USER_NAME: ''
+     RECIEPT_NO: 0
         
     
     })
@@ -31,24 +32,188 @@ adminRouter.get("/cdb_1", (request: Request, response: Response) => {
  return response.json({success:true, msg: 'CREATED'});
      
      })
- 
-adminRouter.get("/cdb_2", (request: Request, response: Response) => {
+
+
+adminRouter.get("/third_party", (request: Request, response: Response) => {
+  
+   Third_party.sync({force: true}).then(() => {
+   //Table created
+  return Third_party.create({
+       
+     PARTY_NAME: 'sam'
+        
+    
+    })
+   })
+
+ return response.json({success:true, msg: 'CREATED'});
+     
+     })
+
+
+
+adminRouter.get("/Expence_detail", (request: Request, response: Response) => {
+  
+   Expence_detail.sync({force: true}).then(() => {
+   //Table created
+  return Expence_detail.create({
+       
+     EXPENCE_ENTRY_NO: 0
+        
+    
+    })
+   })
+
+ return response.json({success:true, msg: 'CREATED'});
+     
+     })
+
+
+adminRouter.get("/Income_expence", (request: Request, response: Response) => {
+  
+   Income_expence.sync({force: true}).then(() => {
+   //Table created
+  return Income_expence.create({
+       
+     EXPENCE_ENTRY_NO: 0
+        
+    
+    })
+   })
+
+ return response.json({success:true, msg: 'CREATED'});
+     
+     })
+
+
+adminRouter.get("/Expence_category", (request: Request, response: Response) => {
+  
+   Expence_category.sync({force: true}).then(() => {
+   //Table created
+  return Expence_category.create({
+       
+     CATEGORY_NAME: 'Purchase'
+        
+    
+    })
+   })
+
+ return response.json({success:true, msg: 'CREATED'});
+     
+     })
+
+adminRouter.get("/invoice_slave", (request: Request, response: Response) => {
+  
+   Invoice_slave.sync({force: true}).then(() => {
+   //Table created
+  return Invoice_slave.create({
+       
+     MASTER_ID: 1
+        
+    
+    })
+   })
+ return response.json({success:true, msg: 'CREATED'});
+     
+     })
+
+
+
+
+
+
+
+
+
+
+
+adminRouter.get("/product_table", (request: Request, response: Response) => {
+  
+   Hb_products.sync({force: true}).then(() => {
+   //Table created
+  return Hb_products.create({
+    
+    
+     PRODUCT_NAME: 'kim'
+        
+    
+    })
+   })
+ return response.json({success:true, msg: 'CREATED'});
+      
+     })
+
+
+adminRouter.get("/cus_table", (request: Request, response: Response) => {
   
    Hb_customers.sync({force: true}).then(() => {
    //Table created
   return Hb_customers.create({
        
-     CUSTOMER_NAME: ''
-        
-    
+     CUSTOMER_NAME: 'kim'
+         
     })
    })
-
  return response.json({success:true, msg: 'CREATED'});
      
      })
 
 
+
+adminRouter.get("/invo_master", (request: Request, response: Response) => {
+  
+   Invoice_master.sync({force: true}).then(() => {
+   //Table created
+  return Invoice_master.create({
+       
+     INVOICE_NUMBER: 1
+     
+    
+    })
+   })
+ return response.json({success:true, msg: 'CREATED'});
+     
+     })
+
+
+adminRouter.get("/user", (request: Request, response: Response) => {
+  
+   Hb_users.sync({force: true}).then(() => {
+   //Table created
+  return Hb_users.create({
+    
+    
+     USER_NAME: 'premji'
+     
+    
+    
+    
+    })
+   })
+ return response.json({success:true, msg: 'CREATED'});
+  
+    
+     })
+     
+     adminRouter.get("/s_a", (request: Request, response: Response) => {
+  
+   Super_admin.sync({force: true}).then(() => {
+   //Table created
+  return Super_admin.create({
+    
+    
+     ADMIN_NAME: 'premji'
+     
+    
+    
+    
+    })
+   })
+ return response.json({success:true, msg: 'CREATED'});
+  
+    
+     })
+     
   
                                                     
 
